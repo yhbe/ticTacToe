42a115baf1f5a0ae7908aee15a277714aa5002a8
SHEF <Shefqetmush@gmail.com>
2022-10-04 22:43:08 -0400

    added two player X &  O on gameboard

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ const playerFactory = (name, symbol) => {
 
 const player1 = playerFactory("P1", "X");
 const player2 = playerFactory("P2", "O");
+let activePlayer = player1;
 
 const gameBoard = (() => {
   const scoreBoard = Array(9).fill("?");
   const container = document.querySelector(".container");
+
   const renderBoard = () => {
     while (container.hasChildNodes()) {
       container.removeChild(container.lastChild);
@@ -23,11 +25,20 @@ const gameBoard = (() => {
       container.append(div);
     });
   };
+
   function updateBoard() {
     const id = this.getAttribute("data-id");
-    scoreBoard[id] = player1.symbol;
+    scoreBoard[id] = activePlayer.symbol;
     renderBoard();
+    updatePlayer();
   }
+
+  const updatePlayer = () => {
+    if (activePlayer == player1) {
+      activePlayer = player2;
+    } else activePlayer = player1;
+  };
+
   return { scoreBoard, renderBoard };
 })();
 
