bd6cfbf3ed00410e3623c72d74b531b6a1f8d28d
SHEF <Shefqetmush@gmail.com>
2022-10-04 22:33:47 -0400

    reset board with new updates

--- script.js
+++ script.js
@@ -9,16 +9,26 @@ const player1 = playerFactory("P1", "X");
 const player2 = playerFactory("P2", "O");
 
 const gameBoard = (() => {
-  let scoreBoard = Array(9).fill("?");
+  const scoreBoard = Array(9).fill("?");
   const container = document.querySelector(".container");
-  const updateBoard = () => {
-    scoreBoard.forEach((score) => {
+  const renderBoard = () => {
+    while (container.hasChildNodes()) {
+      container.removeChild(container.lastChild);
+    }
+    scoreBoard.forEach((score, i) => {
       let div = document.createElement("div");
       div.innerHTML = score;
+      div.setAttribute("data-id", i);
+      div.addEventListener("click", updateBoard);
       container.append(div);
     });
   };
-  return { scoreBoard, updateBoard };
+  function updateBoard() {
+    const id = this.getAttribute("data-id");
+    scoreBoard[id] = player1.symbol;
+    renderBoard();
+  }
+  return { scoreBoard, renderBoard };
 })();
 
-gameBoard.updateBoard();
+gameBoard.renderBoard();
